5cc14898cb98d20e2fe80308094b3e464743aab2
alevinaccia <vinacciaalessandro@gmail.com>
2021-04-09 20:45:25 +0200

    Added category show settins

--- front-end/budget-page/budgetContainerController.js
+++ front-end/budget-page/budgetContainerController.js
@@ -8,17 +8,62 @@ const fillSortBTN = document.querySelector('.fillSortBTN');
 export default class budgetContainerController {
     constructor() {
         this.currentCategoriesShown = [];
-        this.#addListners();
+        this.addListners();
     }
 
     createList(list = this.currentCategoriesShown, param = cat => cat) {
-        this.currentCategoriesShown = list;
+        this.currentCategoriesShown = list.filter(param);
         this.currentCategoriesShown.forEach((category) => {
             this.addElementToList(category)
         })
     }
 
     addElementToList(category) {
+        if(category.budget)
+            this.generateBudgetView(category);
+        else
+            this.generateNonBudgetView(category);
+        
+    }
+
+    addListners() {
+        showAllCheck.addEventListener('input', async () => {
+            container.innerHTML = '';
+            const allCategories = Array.from(await request.getAllCategories());
+            showAllCheck.checked ?
+                (this.createList(allCategories))
+                : (this.createList(allCategories, cat => cat.budget));
+
+        });
+        nameSortBTN.addEventListener('click', () => {
+            container.innerHTML = '';
+            this.currentCategoriesShown.sort(this.nameSort);
+            this.createList();
+        });
+        fillSortBTN.addEventListener('click', () => {
+            container.innerHTML = '';
+            this.currentCategoriesShown.sort(this.fillSort);
+            this.createList();
+        });
+    }
+
+    nameSort(a, b) {
+        if (a.name < b.name)
+            return -1;
+        else if (a.name > b.name)
+            return 1;
+        return 0;
+    }
+
+    fillSort(a, b) {
+        if (a.value < b.value)
+            return 1;
+        else if (a.value > b.value)
+            return -1;
+        return 0;
+    }
+
+    generateBudgetView(category) {
         const budget = document.createElement('div');
         budget.setAttribute('class', 'budget');
         const main = document.createElement('div');
@@ -48,7 +93,7 @@ export default class budgetContainerController {
         colorInput.setAttribute('class', 'colorInput');
         colorInput.value = category.color
         const progress = document.createElement('progress');
-        progress.max = category.budgetValue;
+        progress.max = category.budgetValue || 0;
         progress.value = category.value; //
 
         //Listners
@@ -98,27 +143,62 @@ export default class budgetContainerController {
         container.appendChild(budget);
     }
 
-    #addListners() {
-        showAllCheck.addEventListener('input', this.#showAll);
-        nameSortBTN.addEventListener('click', this.#nameSort);
-        fillSortBTN.addEventListener('click', this.#fillSort);
-    }
+    generateNonBudgetView(category){
+        const budget = document.createElement('div');
+        budget.setAttribute('class', 'budget');
+        const main = document.createElement('div');
+        main.setAttribute('class', 'main');
+        const icons = document.createElement('div');
+        icons.setAttribute('class', 'icons');
+        const i = document.createElement('i');
+        i.setAttribute('class', 'fas fa-trash');
+        i.setAttribute('id', category._id);
+        icons.append(i);
+        const name = document.createElement('span');
+        name.innerText = category.name;
+        const colorContainer = document.createElement('span');
+        const colorInput = document.createElement('input');
+        colorInput.setAttribute('type', 'color');
+        colorInput.setAttribute('class', 'colorInput');
+        colorInput.value = category.color
 
-    async #showAll() {
-        container.innerHTML = '';
-        const allTransactions = Array.from(await request.getAllTransactions());
-        this.createList(allTransactions.forEach(category => this.addElementToList(category)));
-    }
+        //Listners
 
-    #nameSort() {
-        container.innerHTML = '';
-        console.log(this.currentCategoriesShown);
-        this.currentCategoriesShown.sort();
-        this.createList();
-    }   
+        i.addEventListener('click', async () => {
+            let res = await request.removeCategory(category._id);
+            if (res) {
+                let deleteIndex = this.currentCategoriesShown.findIndex(arrCategory => arrCategory._id == category._id);
+                this.currentCategoriesShown.splice(deleteIndex, 1);
+                document.getElementById(`${category._id}`).remove();
+            }
+        })
 
-    #fillSort() {
-        
+        colorInput.addEventListener('input', () => {
+            budget.style.background = colorInput.value;
+            if (this.#checkBrightness(colorInput.value) < 50) {
+                budget.style.color = 'white';
+            } else {
+                budget.style.color = 'black';
+            }
+        });
+
+        colorInput.addEventListener('focusout', async () => {
+            await request.editCategory(budget.id, colorInput.value);      //TODO hardcoded creatorid
+            //TODO call this once the pace is offloaded, and change all categories changed.
+        });
+
+        colorContainer.appendChild(colorInput);
+
+        main.appendChild(name);
+        main.appendChild(colorContainer);
+
+        budget.appendChild(main);
+        budget.appendChild(icons);
+
+        budget.style.background = category.color;
+        budget.setAttribute('id', category._id);
+
+        container.appendChild(budget);
     }
 
     #checkBrightness(color) {
--- front-end/budget-page/pageManger.js
+++ front-end/budget-page/pageManger.js
@@ -3,8 +3,21 @@ import budgetContainerController from "./budgetContainerController.js";
 
 const containerController = new budgetContainerController();
 const request = new Request;
-
+const addBTN = document.querySelector('.addBTN');
 
 window.onload = async () => {
-    containerController.createList(await request.getAllCategories(), cat => cat.vlaue); //TODO creator id hard coded
+    containerController.createList(await request.getAllCategories(), cat => cat.budget); //TODO creator id hard coded
 }
+
+addBTN.addEventListener("click", async () => {
+    if (formContainer.classList.contains("display-none")) {
+        containerController.switchContainer();
+        form.clearForm();
+    } else {
+        let response = await form.sendReq();
+        if (response) {
+            containerController.handleResponse(response);
+            containerController.switchContainer();
+        }
+    }
+});
